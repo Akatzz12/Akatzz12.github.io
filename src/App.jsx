67ac601c95af5f86896e0bfd8312d749ef750ed4
Kapileshwor  Ray <kapileshworray@gmail.com>
2025-09-26 14:07:48 -0500

    template setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Kapileshwor Ray</h1>
+        <p>Welcome to my portfolio website</p>
+        <p>More content will be added soon...</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
