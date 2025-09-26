ee73d40ffc8a8916108fb9412935615351208bae
Kapileshwor  Ray <kapileshworray@gmail.com>
2025-09-26 15:16:04 -0500

    Fix App.jsx to import complete portfolio components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Kapileshwor Ray</h1>
-        <p>Welcome to my portfolio website</p>
-        <p>More content will be added soon...</p>
-      </header>
+      <Navbar />
+      <Home />
     </div>
   )
 }
